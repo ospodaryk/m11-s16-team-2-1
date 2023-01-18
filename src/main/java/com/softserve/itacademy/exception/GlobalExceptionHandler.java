b7e24d28ee54880badb4cdc7c1c892cd6ebb3790
ospodaryk <ospodaryk11@gmail.com>
2023-01-18 10:49:24 +0200

    Added custom access denied exception

--- src/main/java/com/softserve/itacademy/exception/GlobalExceptionHandler.java
+++ src/main/java/com/softserve/itacademy/exception/GlobalExceptionHandler.java
@@ -27,11 +27,11 @@ public class GlobalExceptionHandler {
         return getModelAndView(request, HttpStatus.NOT_FOUND, exception);
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR)
-    public ModelAndView internalServerErrorHandler(HttpServletRequest request, Exception exception) {
-        return getModelAndView(request, HttpStatus.INTERNAL_SERVER_ERROR, exception);
-    }
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR)
+//    public ModelAndView internalServerErrorHandler(HttpServletRequest request, Exception exception) {
+//        return getModelAndView(request, HttpStatus.INTERNAL_SERVER_ERROR, exception);
+//    }
 
     private ModelAndView getModelAndView(HttpServletRequest request, HttpStatus httpStatus, Exception exception) {
         logger.error("Exception raised = {} :: URL = {}", exception.getMessage(), request.getRequestURL());
